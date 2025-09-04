92676d5d57323423d2f07c21f322d6a408444fc4
Vicker <vcalero2001@gmail.com>
2025-09-04 18:24:30 +0200

    STYLE: Added some style to the lateral in preview

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -4,21 +4,39 @@ export default function Preview({formData}) {
   
   return (
     <div className="preview">
-      <div>
-        <h1>General information</h1>
-        <div>
-          <h2>Name : {formData.name} {formData.lastName}</h2>
-          <h2>Phone : {formData.phone}</h2>
-          <h2>Email : {formData.email}</h2>
+      <div className='lateral'>
+
+        <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="currentColor"  className="profile"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 2a5 5 0 1 1 -5 5l.005 -.217a5 5 0 0 1 4.995 -4.783z" /><path d="M14 14a5 5 0 0 1 5 5v1a2 2 0 0 1 -2 2h-10a2 2 0 0 1 -2 -2v-1a5 5 0 0 1 5 -5h4z" /></svg>
+
+        <div className="info">
+
+          <h1>Contact information</h1>
+          <div className='info-content'>
+            <h2>
+              <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="currentColor"  className="info-tag"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M16.52 6a2 2 0 0 1 1.561 .75l3.7 4.625a1 1 0 0 1 0 1.25l-3.7 4.624a2 2 0 0 1 -1.561 .751h-10.52a3 3 0 0 1 -3 -3v-6a3 3 0 0 1 3 -3z" /></svg>
+              {formData.name} {formData.lastName}
+            </h2>
+            <h2>
+              <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="currentColor"  class="info-tag"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 3a1 1 0 0 1 .877 .519l.051 .11l2 5a1 1 0 0 1 -.313 1.16l-.1 .068l-1.674 1.004l.063 .103a10 10 0 0 0 3.132 3.132l.102 .062l1.005 -1.672a1 1 0 0 1 1.113 -.453l.115 .039l5 2a1 1 0 0 1 .622 .807l.007 .121v4c0 1.657 -1.343 3 -3.06 2.998c-8.579 -.521 -15.418 -7.36 -15.94 -15.998a3 3 0 0 1 2.824 -2.995l.176 -.005h4z" /></svg>
+              {formData.phone}
+            </h2>
+            <h2>
+              <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="currentColor"  class="info-tag"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M22 7.535v9.465a3 3 0 0 1 -2.824 2.995l-.176 .005h-14a3 3 0 0 1 -2.995 -2.824l-.005 -.176v-9.465l9.445 6.297l.116 .066a1 1 0 0 0 .878 0l.116 -.066l9.445 -6.297z" /><path d="M19 4c1.08 0 2.027 .57 2.555 1.427l-9.555 6.37l-9.555 -6.37a2.999 2.999 0 0 1 2.354 -1.42l.201 -.007h14z" /></svg>
+              {formData.email}
+            </h2>
+          </div>
         </div>
+
       </div>
       
-      <div>
-        <h1>Educational formation</h1>
-      </div>
-      
-      <div>
-        <h1>Practice experience</h1>
+      <div className="main">
+        <div>
+          <h1>Educational formation</h1>
+        </div>
+        
+        <div>
+          <h1>Practice experience</h1>
+        </div>
       </div>
       
     </div>
